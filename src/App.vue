f8d37b9cbcb285e508dd2d01982fb095ad3e44d9
myzi726 <mzi726@naver.com>
2024-01-20 22:38:00 +0900

    v-for 문법

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,36 @@
 <template>
+
+  <div class="menu">
+    <a v-for="name in navMenu" :key="name"> {{ name }} </a>
+
+  </div>
+
   <img alt="Vue logo" src="./assets/logo.png">
-  
-  <HelloWorld msg="Hello"/>
+  <div>
+    <h4>{{ products[0] }}</h4>
+    <p>60 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>80 만원</p>
+  </div>
+    <div>
+    <h4>{{ products[2] }}</h4>
+    <p>50 만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data(){
+    return {
+      navMenu : ['Home', 'Shop', 'About'],
+      products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
+     }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -23,5 +43,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
