43ed2613400f908492af401c68b04b6a1651fb27
myzi726 <mzi726@naver.com>
2024-01-20 23:18:24 +0900

    eventHandler2

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,19 @@
   <div>
     <h4>{{products[0]}}</h4>
     <p>50만원</p>
-    <button @click="신고수+=1">허위매물신고</button> <span>신고수 : {{신고수}}</span>
+    <button @click="신고수[0]++">허위매물신고</button> <span>신고수 : {{신고수[0]}}</span>
   </div>
   <div>
     <h4>{{products[1]}}</h4>
     <p>60만원</p>
+    <button @click="신고수[1]++">허위매물신고</button> <span>신고수 : {{신고수[1]}}</span>
+
   </div>
     <div>
     <h4>{{products[2]}}</h4>
     <p>70만원</p>
+    <button @click="신고수[2]++">허위매물신고</button> <span>신고수 : {{신고수[2]}}</span>
+
   </div>
 </template>
 
@@ -27,9 +31,16 @@ export default {
     return {
       navMenu : ['Home', 'Shop', 'About'],
       products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
-      신고수 : 0,
+      신고수 : [0,0,0],
      }
   },
+
+  methods: {
+    // increase() {
+    //   this.신고수 += 1;
+    // }
+  },
+
   components: {
   }
 }
