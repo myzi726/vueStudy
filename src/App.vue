24b811d66667557f43a2c76a7b7263e52b964f28
myzi726 <mzi726@naver.com>
2024-01-21 10:37:27 +0900

    modal

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,46 @@
 <template>
 
+ <!-- 상세페이지 모달창 -->
+  <div class="black-bg" v-if="modal == true">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <p>상세페이지 내용임</p>
+      <button @click="modal = false">닫기</button>
+    </div>
+  </div>
+
+
+
+
   <div class="menu">
     <a v-for="a in navMenu" :key="a"> {{ a }} </a>
   </div>
 
+
+
+
+
   <div>
-    <h4>{{products[0]}}</h4>
+    <img src="./assets/room0.jpg" class="room-img">
+    <h4 @click="modal = true">{{products[0]}}</h4>
     <p>50만원</p>
-    <button @click="신고수[0]++">허위매물신고</button> <span>신고수 : {{신고수[0]}}</span>
+    <button @click="신고수[0]++">허위매물신고</button>
+    <span> 신고수 : {{신고수[0]}}</span>
   </div>
   <div>
+    <img src="./assets/room1.jpg" class="room-img">
     <h4>{{products[1]}}</h4>
     <p>60만원</p>
-    <button @click="신고수[1]++">허위매물신고</button> <span>신고수 : {{신고수[1]}}</span>
+    <button @click="신고수[1]++">허위매물신고</button> 
+    <span> 신고수 : {{신고수[1]}}</span>
 
   </div>
     <div>
+    <img src="./assets/room2.jpg" class="room-img">
     <h4>{{products[2]}}</h4>
     <p>70만원</p>
-    <button @click="신고수[2]++">허위매물신고</button> <span>신고수 : {{신고수[2]}}</span>
+    <button @click="신고수[2]++">허위매물신고</button>
+    <span> 신고수 : {{신고수[2]}}</span>
 
   </div>
 </template>
@@ -29,6 +51,7 @@ export default {
   name: 'App',
   data(){
     return {
+      modal : false,
       navMenu : ['Home', 'Shop', 'About'],
       products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
       신고수 : [0,0,0],
@@ -47,6 +70,26 @@ export default {
 </script>
 
 <style>
+body {
+  margin : 0
+}
+
+div {
+  box-sizing : border-box;
+}
+
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed; padding: 20px;
+}
+
+.white-bg {
+  width: 100%; background: white;
+   border-radius: 8px;
+   padding: 20px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -64,5 +107,10 @@ export default {
 .menu a {
   color: white;
   padding: 10px;
+}
+
+.room-img{
+  width: 100%;
+  margin-top: 40px;
 }
 </style>
