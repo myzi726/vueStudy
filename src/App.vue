46f85b9f93561a785cfcbd62d7e8d38977fdfa2a
myzi726 <mzi726@naver.com>
2024-01-22 21:33:23 +0900

    import/export

--- src/App.vue
+++ src/App.vue
@@ -10,47 +10,29 @@
   </div>
 
 
-
-
   <div class="menu">
     <a v-for="a in navMenu" :key="a"> {{ a }} </a>
   </div>
 
 
-
-
-
-  <div>
-    <img src="./assets/room0.jpg" class="room-img">
-    <h4 @click="modal = true">{{products[0]}}</h4>
-    <p>50만원</p>
-    <button @click="신고수[0]++">허위매물신고</button>
-    <span> 신고수 : {{신고수[0]}}</span>
+  <div v-for="i in 원룸들" :key="i">
+    <img :src="원룸들[i].image" class="room-img">
+    <h4>{{원룸들[i].title}}</h4>
+    <p>{{원룸들[i].price}}</p>
   </div>
-  <div>
-    <img src="./assets/room1.jpg" class="room-img">
-    <h4>{{products[1]}}</h4>
-    <p>60만원</p>
-    <button @click="신고수[1]++">허위매물신고</button> 
-    <span> 신고수 : {{신고수[1]}}</span>
 
-  </div>
-    <div>
-    <img src="./assets/room2.jpg" class="room-img">
-    <h4>{{products[2]}}</h4>
-    <p>70만원</p>
-    <button @click="신고수[2]++">허위매물신고</button>
-    <span> 신고수 : {{신고수[2]}}</span>
-
-  </div>
 </template>
 
 <script>
+
+import data from './assets/oneroom.js'
+
 
 export default {
   name: 'App',
   data(){
     return {
+      원룸들 : data,
       modal : false,
       navMenu : ['Home', 'Shop', 'About'],
       products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
